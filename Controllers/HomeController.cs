3a4d011171aab9a5e656727690913be50b8b46b2
Victor Pacheco <vpacheco26@yahoo.com.br>
2024-05-20 23:57:27 -0300

    Atualizando projeto

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Curso_Asp_MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,27 @@ namespace Curso_Asp_MVC.Controllers
     {
         // GET: Home
         public ActionResult Index()
+        {
+            var pessoa = new Pessoa()
+            {
+                Id = 1,
+                Nome = "Vanderlei",
+                Tipo = "Adm"
+            };
+
+            ViewBag.Idddd = pessoa.Id;
+            ViewData["Nome"] = pessoa.Nome;
+            ViewData["Tipo"] = pessoa.Tipo;
+
+            return View();
+        }
+
+        public ActionResult Contatos() 
+        {
+            return View();
+        }
+
+        public ActionResult About() 
         {
             return View();
         }
---
+++ Models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Curso_Asp_MVC.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+    }
+}
